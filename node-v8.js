8c80b6108f5fccdb32c115b92bf151c22470b545
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2017-06-12 06:03:27 +0100

    added support for Node.js 8

---
+++ node-v8.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+
+    "extends": [
+
+        "./node-v8.js",
+        "./es2017.js" // TODO: As of 06/2017, support is at 50%+
+
+    ],
+
+    "rules": {
+
+        "node/no-unsupported-features": [ "error", { "version": 8 } ]
+
+    }
+
+};
