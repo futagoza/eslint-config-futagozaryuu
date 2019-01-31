d03efe9aaf7b4ada3f505707cd70138a72192cfb
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-01-31 04:54:36 +0000

    Use string based node version range

--- node-v4.js
+++ node-v4.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const config = [ "error", { "version": ">= 4" } ];
+
 module.exports = {
 
     "extends": [
@@ -14,9 +16,9 @@ module.exports = {
         "prefer-rest-params": 0,
         "prefer-spread": 0,
 
-        "node/no-unsupported-features/es-builtins": [ "error", { "version": 4 } ],
-        "node/no-unsupported-features/es-syntax": [ "error", { "version": 4 } ],
-        "node/no-unsupported-features/node-builtins": [ "error", { "version": 4 } ],
+        "node/no-unsupported-features/es-builtins": config,
+        "node/no-unsupported-features/es-syntax": config,
+        "node/no-unsupported-features/node-builtins": config,
 
     }
 
--- node-v6.js
+++ node-v6.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const config = [ "error", { "version": ">= 6" } ];
+
 module.exports = {
 
     "extends": [
@@ -13,9 +15,9 @@ module.exports = {
 
         "no-buffer-constructor": "error",
 
-        "node/no-unsupported-features/es-builtins": [ "error", { "version": 6 } ],
-        "node/no-unsupported-features/es-syntax": [ "error", { "version": 6 } ],
-        "node/no-unsupported-features/node-builtins": [ "error", { "version": 6 } ],
+        "node/no-unsupported-features/es-builtins": config,
+        "node/no-unsupported-features/es-syntax": config,
+        "node/no-unsupported-features/node-builtins": config,
 
     }
 
