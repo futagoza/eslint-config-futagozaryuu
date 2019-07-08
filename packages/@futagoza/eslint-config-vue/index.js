160dc591d05b8c45f03b81b88d7531b2a12bb594
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-07-08 21:36:57 +0100

    Create @futagoza/eslint-config-vue

---
+++ packages/@futagoza/eslint-config-vue/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+
+    "overrides": [ {
+
+        "files": [ "*.vue" ],
+        "extends": [
+
+            "plugin:vue/recommended",
+
+        ],
+
+        "rules": {
+
+            "strict": "off",
+
+            "vue/html-indent": [ "error", 4 ],
+            "vue/singleline-html-element-content-newline": 0,
+
+        },
+
+    } ],
+
+};
