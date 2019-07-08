0ac2ef07cff4ed06b1212fb846734b4bf7bcf4b8
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-07-08 21:16:43 +0100

    Add deprected rules for eslint-plugin-node

---
+++ packages/@futagoza/eslint-config-node/deprecated.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+
+    "rules": {
+
+        /**
+         * Disallow unsupported ECMAScript features on the specified version.
+         * 
+         * This rule `node/no-unsupported-features` was replaced by `node/no-unsupported-features/es-builtins`
+         * and `node/no-unsupported-features/es-syntax`.
+         *
+         * @deprecated eslint-plugin-node 7.0.0
+         * @see https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-unsupported-features.md
+         */
+        "node/no-unsupported-features": 0,
+
+    },
+
+};
