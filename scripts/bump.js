eeda0283c1ba4a5c4c7f775c2f76e074dfc1d9f4
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-07-13 02:35:50 +0100

    Update gitting features for bumper
    - Update command runner
    - Ensure an error is thrown on un-clean repo
    - Add changed metafiles to commit

--- scripts/bump.js
+++ scripts/bump.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const child = require( "@futagoza/child-process" );
-const cp2 = require( "child_process" ).promise;
 const { log, color } = require( "@futagoza/cli-utils" );
 const EachSerially = require( "bluebird" ).each;
 const GetYarnWorkspaces = require( "get-yarn-workspaces" );
@@ -11,36 +10,41 @@ const PromptAsync = require( "prompts" );
 const $repository = process.cwd();
 const $configurations = GetYarnWorkspaces( $repository );
 
-function forEach( $command, $cb ) {
+function spawn( $command, $cb, cwd = $repository ) {
 
     const fn = $cb ? child.exec : child.run;
+    const p = fn( $command, { cwd } );
 
-    return EachSerially( $configurations, $cwd => {
-
-        const p = fn( $command, { cwd: $cwd } );
-
-        return $cb ? p.then( $cb ) : p;
-
-    } );
+    return $cb ? p.then( $cb ) : p;
 
 }
 
-function exec( $command ) {
+function forEach( $command, $cb ) {
 
-    return child.run( $command, { cwd: $repository } );
+    return EachSerially( $configurations, $cwd => spawn( $command, $cb, $cwd ) );
 
 }
 
 ( async () => {
 
-    // 1. Ask for new version first (duh)
+    // 01. We require all tracked files to be commited
+    await spawn( "git status --untracked-files=no --porcelain", $result => {
+
+        if ( $result.stdout.trim() === "" ) return;
+
+        log.error( "Git working directory not clean!\n\n", $result.stdout );
+        process.exit( 1 );
+
+    } );
+
+    // 02. Ask for new version first (duh)
     const { $newVersion } = await PromptAsync( {
         type: "text",
         name: "$newVersion",
         message: "Version number",
     } );
 
-    // 2. Update the version field in every workspace package
+    // 03. Update the version field in every workspace package
     await forEach( `npm --no-git-tag-version version ${ $newVersion }`, $result => {
 
         const $name = require( $result.options.cwd + "/package.json" ).name;
@@ -50,20 +54,23 @@ function exec( $command ) {
 
     } );
 
-    // 3. Sync the version of any dependency that is also a workspace package
+    // 04. Sync the version of any dependency that is also a workspace package
     SyncYarnWorkspaces( $repository );
 
-    // 4. Just to be sure, run the tests
-    await exec( `npm run test` );
+    // 05. Just to be sure, run the tests
+    await spawn( `npm run test` );
 
-    // 5. Commit the changes, while also tagging the commit
-    await exec( `git commit -m ${ $newVersion }` );
-    await exec( `git tag v${ $newVersion } -m ${ $newVersion }` );
+    // 06. Add newly updated `package.json` files to the current commit
+    await spawn( `git add -u` );
 
-    // 6. Publish every workspace package to NPM
+    // 07. Commit the changes, while also tagging the commit
+    await spawn( `git commit -m ${ $newVersion }` );
+    await spawn( `git tag v${ $newVersion } -m ${ $newVersion }` );
+
+    // 08. Publish every workspace package to NPM
     await forEach( `npm publish --tag latest` );
 
-    // 7. Push the changes to `https://github.com/futagoza/eslint-config-futagozaryuu/`
-    await exec( `git push --follow-tags` );
+    // 09. Push the changes to `https://github.com/futagoza/eslint-config-futagozaryuu/`
+    await spawn( `git push --follow-tags` );
 
 } )();
