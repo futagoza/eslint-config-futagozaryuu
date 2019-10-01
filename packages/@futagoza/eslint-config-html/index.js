cbc77f30b0f052b20e1bd3e15da64f5592ba8b16
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-10-01 19:14:26 +0100

    Tidy and reorganize html/* settings

--- packages/@futagoza/eslint-config-html/index.js
+++ packages/@futagoza/eslint-config-html/index.js
@@ -2,61 +2,37 @@
 
 const globals = require( "globals" );
 
+// https://github.com/BenoitZugmeyer/eslint-plugin-html#settings
+const html = {
+
+    "html-extensions": [ ".htm", ".html", ".phtml" ],
+    "xml-extensions": [ ".xhtml", ".xml" ],
+
+    "indent": "+4",
+    "report-bad-indent": "error",
+
+    "javascript-mime-types": [
+
+        "application/javascript",
+        "module",
+        "text/babel",
+        "text/ecmascript-6",
+        "text/javascript",
+        "text/jsx",
+        "text/typescript",
+
+    ],
+
+};
+
 module.exports = {
 
     "overrides": [ {
 
-        "files": [
-
-            "*.html",
-            "*.htm",
-            "*.phtml",
-            "*.xhtml",
-            "*.xml",
-
-        ],
-
+        "files": [ "*.html", "*.htm", "*.phtml", "*.xhtml", "*.xml" ],
         "plugins": [ "html" ],
-
         "globals": Object.assign( {}, globals.browser, globals.worker ),
-
-        "rules": {
-
-            // https://github.com/BenoitZugmeyer/eslint-plugin-html#settings
-
-            "html/html-extensions": [
-
-                ".htm",
-                ".html",
-                ".phtml",
-
-            ],
-
-            "html/xml-extensions": [
-
-                ".xhtml",
-                ".xml",
-
-            ],
-
-            "html/indent": "+4",
-            "html/report-bad-indent": "error",
-
-            "html/javascript-mime-types": [
-
-                "application/babel",
-                "application/javascript",
-                "module",
-                "text/babel",
-                "text/ecmascript-6",
-                "text/javascript",
-                "text/jsx",
-                "text/typescript",
-                "text/tsx",
-
-            ],
-
-        },
+        "settings": { html },
 
     } ],
 
