21352cf149f0c656901e8a778a3c5afa512a3f69
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2020-06-07 16:20:58 +0100

    Normalize plugin settings

--- packages/@futagoza/eslint-config-html/index.js
+++ packages/@futagoza/eslint-config-html/index.js
@@ -1,40 +1,41 @@
 "use strict";
 
-// https://github.com/BenoitZugmeyer/eslint-plugin-html#settings
-const html = {
-
-    "html-extensions": [ ".htm", ".html", ".phtml" ],
-    "xml-extensions": [ ".xhtml", ".xml" ],
-
-    "indent": "+4",
-    "report-bad-indent": "error",
-
-    "javascript-mime-types": [
-
-        "application/javascript",
-        "module",
-        "text/babel",
-        "text/ecmascript-6",
-        "text/javascript",
-        "text/jsx",
-        "text/typescript",
-
-    ],
-
-};
-
 module.exports = {
 
     "overrides": [ {
 
         "files": [ "*.html", "*.htm", "*.phtml", "*.xhtml", "*.xml" ],
+
         "extends": [
 
             "@futagoza/globals/browser",
 
         ],
+
         "plugins": [ "html" ],
-        "settings": { html },
+
+        // https://github.com/BenoitZugmeyer/eslint-plugin-html#settings
+        "settings": {
+
+            "html/html-extensions": [ ".htm", ".html", ".phtml" ],
+            "html/xml-extensions": [ ".xhtml", ".xml" ],
+
+            "html/indent": "+4",
+            "html/report-bad-indent": "error",
+
+            "html/javascript-mime-types": [
+
+                "application/javascript",
+                "module",
+                "text/babel",
+                "text/ecmascript-6",
+                "text/javascript",
+                "text/jsx",
+                "text/typescript",
+
+            ],
+
+        },
 
     } ],
 
--- packages/@futagoza/eslint-config-svelte/index.js
+++ packages/@futagoza/eslint-config-svelte/index.js
@@ -1,16 +1,5 @@
 "use strict";
 
-// https://github.com/sveltejs/eslint-plugin-svelte3#configuration
-const settings = {
-
-    // "svelte3/ignore-warnings": () => false,
-    // "svelte3/compiler-options": { generate: false },
-    // "svelte3/ignore-styles": () => false,
-    // "svelte3/named-blocks": false,
-    // "svelte3/compiler": require( "svelte/compiler" ),
-
-};
-
 module.exports = {
 
     "overrides": [
@@ -26,7 +15,17 @@ module.exports = {
             ],
             "plugins": [ "svelte3" ],
             "processor": "svelte3/svelte3",
-            settings,
+
+            // https://github.com/sveltejs/eslint-plugin-svelte3#configuration
+            "settings": {
+
+                // "svelte3/ignore-warnings": () => false,
+                // "svelte3/compiler-options": { generate: false },
+                // "svelte3/ignore-styles": () => false,
+                // "svelte3/named-blocks": false,
+                // "svelte3/compiler": require( "svelte/compiler" ),
+
+            },
 
         },
 
