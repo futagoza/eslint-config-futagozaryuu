f6f4f68b5daee59e0f0e72341709208d70de9b11
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2020-07-09 21:27:15 +0100

    Disable typescript-eslint/no-unsafe-* rules

--- packages/@futagoza/eslint-config/index.js
+++ packages/@futagoza/eslint-config/index.js
@@ -11,4 +11,16 @@ module.exports = {
 
     ],
 
+    "overrides": [
+        {
+            "files": [ "*.ts", "*.tsx" ],
+            "rules": {
+                "@typescript-eslint/no-unsafe-assignment": "off",
+                "@typescript-eslint/no-unsafe-call": "off",
+                "@typescript-eslint/no-unsafe-member-access": "off",
+                "@typescript-eslint/no-unsafe-return": "off",
+            },
+        },
+    ],
+
 };
