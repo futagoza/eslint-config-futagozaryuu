b145b2167320cff7534fee47dbb710689d078918
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2021-05-23 00:51:20 +0100

    lazy-load ts when linting svelte files

--- packages/@futagoza/eslint-config-svelte.ts/index.js
+++ packages/@futagoza/eslint-config-svelte.ts/index.js
@@ -23,7 +23,7 @@ module.exports = {
 
             "settings": {
 
-                "svelte3/typescript": require( "typescript" ),
+                "svelte3/typescript": () => require( "typescript" ),
 
             },
 
