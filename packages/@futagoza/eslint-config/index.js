af06986ee27bf2575f11c6d72e1ac881edfab473
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2022-11-27 23:16:15 +0000

    Drop support for Node.js 12

--- packages/@futagoza/eslint-config/index.js
+++ packages/@futagoza/eslint-config/index.js
@@ -5,7 +5,7 @@ module.exports = {
     "extends": [
 
         "@futagoza/ignore",
-        "@futagoza/node/12",
+        "@futagoza/node/14",
         "@futagoza/typescript",
         "@futagoza/dev/test",
 
