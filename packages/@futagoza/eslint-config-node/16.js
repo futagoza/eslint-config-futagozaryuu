97e7adddf8faf14355c80029e9de053bb7b0b3c9
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2022-11-30 22:02:41 +0000

    Add configs for Node.js LTS 16 & 18

---
+++ packages/@futagoza/eslint-config-node/16.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const { CreateNodeConfig } = require( "./internal" );
+
+module.exports = CreateNodeConfig( {
+
+    versions: {
+        es: 2021,
+        node: 16,
+    },
+
+} );
---
+++ packages/@futagoza/eslint-config-node/18.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const { CreateNodeConfig } = require( "./internal" );
+
+module.exports = CreateNodeConfig( {
+
+    versions: {
+        es: 2022,
+        node: 18,
+    },
+
+} );
